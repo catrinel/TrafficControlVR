382cb6c336dc29a734c778663e2eb634809ccff9
ASUSUser <carausu.catrinel@gmail.com>
2018-01-17 00:03:15 +0200

    Initial setup: Intersection roads and basic moving cars

---
+++ Assets/scripts/CarEngine.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour {
+
+    public Transform path;
+    public float maxSteerAngle = 45f;
+
+    private List<Transform> nodes;
+    private int currentNode = 0;
+
+
+    public WheelCollider FRWheel;
+    public WheelCollider FLWheel;
+
+	// Use this for initialization
+	void Start () {
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != transform)
+                nodes.Add(pathTransforms[i]);
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        ApplySteer();
+        Drive();
+        CheckWaypointDistance();
+	}
+
+    void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        FLWheel.steerAngle = newSteer;
+        FRWheel.steerAngle = newSteer;
+    }
+
+    void Drive()
+    {
+        FLWheel.motorTorque = 2000f;
+        FRWheel.motorTorque = 2000f;
+    }
+
+    void CheckWaypointDistance()
+    {
+        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 0.5f)
+        {
+            if(currentNode == nodes.Count - 1)
+            {
+                // destroy car
+                return;
+            }
+            currentNode++;
+        }
+    }
+}
---
+++ Assets/scripts/Path.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+    public Color lineColor;
+
+    private List<Transform> nodes = new List<Transform>();
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = lineColor;
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for(int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != transform)
+                nodes.Add(pathTransforms[i]);
+        }
+
+        for(int i = 1; i < nodes.Count; i++)
+        {
+            Vector3 currentNode = nodes[i].position;
+            Vector3 prevNode = prevNode = nodes[i - 1].position;
+
+            Gizmos.DrawLine(prevNode, currentNode);
+        }
+    }
+}
